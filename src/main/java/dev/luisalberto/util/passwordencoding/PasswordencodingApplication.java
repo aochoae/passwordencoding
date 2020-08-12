30edbe8c8a4e0f1cc9efc574e80465b9b66adac0
Luis A. Ochoa <alberto.0x7c3@gmail.com>
2020-08-11 19:46:31 -0500

    First commit

---
+++ src/main/java/dev/luisalberto/util/passwordencoding/PasswordencodingApplication.java
@@ -0,0 +1,13 @@
+package dev.luisalberto.util.passwordencoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordencodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordencodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/luisalberto/util/passwordencoding/PasswordencodingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luisalberto.util.passwordencoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordencodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
